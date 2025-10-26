b2610054dd4c6338882a2b4e94cc0935188f5a69
Abdelrahman el-bahrawy <manobahrawy1@gmail.com>
2025-10-26 16:35:00 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 import { Hero } from "@/components/Hero";
 import { Navbar } from "@/components/Navbar";
 import { QuoteGenerator } from "@/components/QuoteGenerator";
-import { CallToAction } from "@/components/CallToAction";
 import { Features } from "@/components/Features";
 import { Footer } from "@/components/Footer";
 
